26c51a8a2b4d325b5c8449dd16a3763f68accacf
potato <845291072@qq.com>
2024-01-01 22:29:44 +0800

    feat: 裁剪框保持比例移动

--- src/layout/main/components/crop.vue
+++ src/layout/main/components/crop.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="crop-box">
+  <div ref="wrapRef" class="wrap-box"></div>
+  <div
+    ref="cropRef"
+    class="crop-box"
+    :style="{
+      transform: `translate(${wrapWidth * Rw}px, ${wrapHeight * Rh}px)`,
+    }"
+  >
     <div class="scale-point tl"></div>
     <div class="scale-point tm"></div>
     <div class="scale-point tr"></div>
@@ -22,18 +29,77 @@ defineProps({
     default: 0,
   },
 })
+
+const cropRef = ref()
+const wrapRef = ref()
+let cropBoxTransX = 0
+let cropBoxTransY = 0
+let wrapBoxResizeObserver: ResizeObserver
+const wrapWidth = ref()
+const wrapHeight = ref()
+const Rw = ref(0)
+const Rh = ref(0)
+
+let cropCanmove = ref(false)
+
+onMounted(() => {
+  wrapWidth.value = wrapRef.value.clientWidth
+  wrapHeight.value = wrapRef.value.clientHeight
+  wrapBoxResizeObserver = new ResizeObserver((e) => {
+    wrapWidth.value = Math.floor(e[0].contentRect.width)
+    wrapHeight.value = Math.floor(e[0].contentRect.height)
+  })
+  wrapBoxResizeObserver.observe(unref(wrapRef), {
+    box: 'content-box',
+  })
+  cropRef.value.addEventListener('mousedown', mouseDown)
+  cropRef.value.addEventListener('mousemove', mouseMove)
+  cropRef.value.addEventListener('mouseup', mouseUp)
+  console.log(cropRef)
+})
+
+function mouseDown() {
+  cropCanmove.value = true
+}
+
+function mouseUp() {
+  cropCanmove.value = false
+}
+
+function mouseMove(e: MouseEvent) {
+  if (!cropCanmove.value) return
+  const x = cropBoxTransX + e.movementX
+  const y = cropBoxTransY + e.movementY
+  const maxX = wrapWidth.value - cropRef.value.clientWidth
+  const maxY = wrapHeight.value - cropRef.value.clientHeight
+  if (x <= 0 || x >= maxX || y <= 0 || y >= maxY) return
+  cropBoxTransX = x
+  cropBoxTransY = y
+  Rw.value = Math.round((cropBoxTransX / wrapWidth.value) * 100) / 100
+  Rh.value = Math.round((cropBoxTransY / wrapHeight.value) * 100) / 100
+}
 </script>
 
 <style lang="scss" scoped>
-.crop-box {
+.wrap-box {
   box-sizing: content-box;
-  border: 1px solid var(--my-color);
   position: absolute;
   left: 0;
   top: 0;
   right: 0;
   bottom: 0;
   background-color: rgba(1, 1, 1, 0.8);
+}
+.crop-box {
+  box-sizing: border-box;
+  width: 50%;
+  height: 50%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  border: 1px solid var(--my-color);
+}
+.cliping {
   clip-path: polygon(20px 20px, 100px 20px, 100px 100px, 20px 100px);
 }
 
@@ -43,11 +109,31 @@ defineProps({
   height: 8px;
   border: 1px solid var(--my-color);
   transform: translate(-50%, -50%);
+  background-color: rgba(1, 1, 1, 1);
+}
+.scale-point:hover {
+  background-color: var(--my-color);
+}
+.tm:hover,
+.bm:hover {
+  cursor: ns-resize;
+}
+.cl:hover,
+.cr:hover {
+  cursor: ew-resize;
+}
+.tl:hover,
+.br:hover {
+  cursor: nwse-resize;
+}
+.tr:hover,
+.bl:hover {
+  cursor: nesw-resize;
 }
 .tl,
 .tm,
 .tr {
-  top: 20px;
+  top: 0;
 }
 .cl,
 .cr {
@@ -61,7 +147,7 @@ defineProps({
 .tl,
 .cl,
 .bl {
-  left: 0px;
+  left: 0;
 }
 .tm,
 .bm {
